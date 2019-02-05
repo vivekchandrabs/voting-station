9ad6a9e0cc737bdbcbca31a1083e7db4089a2468
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-02-05 17:15:46 +0530

    add forms to the html page

--- poll/settings.py
+++ poll/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c2dsd0uqbsv27mlh5_%*o6$ojx@gj-c_2bsnttogmt88+o^3%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 LOGIN_URL = '/'
 
 ALLOWED_HOSTS = []
