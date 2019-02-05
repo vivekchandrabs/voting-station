02c8762c73c0bd4f76cbda7b5977733d09bb1354
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-02-05 12:51:03 +0530

    git push heroku master

---
+++ polling/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,redirect
+from polling.models import Question,Choice
+
+# Create your views here.
+
+def question(request):
+
+	question = Question.objects.all().first()
+	choice_set = Choice.objects.filter(question = question)
+	return render(request, "index.html", {'question':question, "choice_set":choice_set})
+
+def answer(request, questionid, choiceid):
+
+	question = Question.objects.get(pk = questionid)
+	choice = Choice.objects.get(pk = choiceid, question=question)
+	choice.votes+=1
+	choice.save()
+	return redirect('/thankyou/')
+
+def thankyou(request):
+	return render(request, "thankyou.html")
+
+
+
+
